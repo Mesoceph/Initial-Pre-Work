8d9eb1182d784ed34bbf9b84460fd07b3d2f5ea0
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 17:35:12 -0600

    Declare and assign new variables
    Declare and assign new variables using camelCase.  Add a comment explaining what this lesson covers about case-sensitivity and use of camelCase for variable names.

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,11 @@
+//This lesson reviews the fact that JavaScript variables are case-sensitive and that accepted best practice is to use camelCase for variable names.
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
