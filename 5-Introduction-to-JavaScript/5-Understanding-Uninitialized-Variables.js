983976dc58731fa3d506bd5ca2371b1e4559a4ce
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 17:25:07 -0600

    Initialize three new variables
    Initialize three new variables.  Variables a and b have numerical values so that mathematical operations can be performed on them.  Variable c is a string, and so can be concatenated with other strings later.  Add a comment describing what this lesson teaches about initialization and the data types "undefined", "number", and "string".

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,12 @@
+//This lesson reviews the fact that uninitialized values have a value of undefined, which is one of the seven JavaScript data types.  This lesson also demonstrates that variables with values of the "number" data type can be used in mathematical operations, while variable types of the "string" data type can be concatenated with other strings.  However, neither of these operations can be performed on an uninitialized variable of data type "undefined".
+
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
