3fd173be592339519cdddb0ad1a24b9e850bc7ba
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 17:58:38 -0600

    Adjust myVar value using decrement operator
    Decrease the value of the myVar variable by one using the decrement operator (--).  Add a comment explaining what I learned in this lesson about using the decrement operator in JavaScript.

---
+++ 5-Introduction-to-JavaScript/12-Decrement-a-Number-with-JavaScript.js
@@ -0,0 +1,6 @@
+//This lesson covers the decrement operator in JavaScript, which is the opposite of the increment operator.  Using the decrement operator decreases the value of the variable by 1.
+
+var myVar = 11;
+
+// Only change code below this line
+myVar--;
