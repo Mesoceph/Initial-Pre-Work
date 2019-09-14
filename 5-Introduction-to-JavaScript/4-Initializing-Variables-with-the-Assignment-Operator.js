83c1c694b36c15deb1b1ac89d71a087d9664a8ba
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 17:18:06 -0600

    Initialize new variable a to value = 9
    Create a new variable and initialize it to give it the initial value of 9.  Add a comment explaining how variables are initialized in JavaScript.

---
+++ 5-Introduction-to-JavaScript/4-Initializing-Variables-with-the-Assignment-Operator.js
@@ -0,0 +1,8 @@
+//This lesson reviews how to declare a variable and initialize its value in the same line using the syntax: var [variable name] = [variable value];
+
+// Example
+var ourVar = 19;
+
+// Only change code below this line
+
+var a = 9;
