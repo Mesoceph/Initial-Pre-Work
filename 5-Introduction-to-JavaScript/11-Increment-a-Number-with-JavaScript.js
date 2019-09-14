e32d7540c6672dd209c97b4556447d6d681c3ed5
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 17:51:53 -0600

    Increment variable myVar
    Increment variable myVar to increase its value by one.  Add a comment explaining what this lesson teaches about using the increment operator in JavaScript.

---
+++ 5-Introduction-to-JavaScript/11-Increment-a-Number-with-JavaScript.js
@@ -0,0 +1,6 @@
+//This lesson covers the increment operator (++) in JavaScript.  A variable name followed by the increment operator will increase the value of the variable by one.
+
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
