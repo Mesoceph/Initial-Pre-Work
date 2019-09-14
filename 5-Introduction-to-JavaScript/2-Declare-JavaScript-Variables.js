84145f2e9d23b36232bbb6aaae8a517665b3b040
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 17:08:44 -0600

    Declare variable myName
    Declare the variable myName.  Add a comment explaining what I learned in this lesson about declaring variables with the keyword "var" and about JS data types.

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,8 @@
+//This lesson reviewed how to declare variables in JavaScript using the keyword var followed by the variable name.  Variable names can contain letters, numbers, $, and _, but cannot start with a number and cannot contain spaces.  This lesson also reviewed the seven data types used by Javascript: strings, numbers, undefined, null, boolean, symbol, and object.
+
+// Example
+var ourName;
+
+// Declare myName below this line
+
+var myName;
