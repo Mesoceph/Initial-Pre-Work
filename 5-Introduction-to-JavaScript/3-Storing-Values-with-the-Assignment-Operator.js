db0da13bcb7e2d83f1d60d2c8fcca32eb1fb0e4e
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 17:13:32 -0600

    Assign values to variables a and b
    Assign the value 7 to variables a and b.  Also add a comment explaining what this lesson covered on variable assignment.

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,10 @@
+//This lesson reviews how to assign values to variables using the "=" symbol.  The value can be assigned directly using a specified value (i.e. var1 = 8), or can be set to the current contents of another variable (i.e. var1 = var2;).
+
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+
+a = 7;
+b = a;
