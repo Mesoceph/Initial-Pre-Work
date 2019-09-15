7936eb58f14297c661ca28aa748174d6c7473edb
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 22:49:23 -0600

    Create variable for 1st array value
    Create a new variable, myData, and initialize it with the first value in the array myArray.  Add a comment explaining how bracket notation can be used to access data stored in arrays.

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,12 @@
+//In this lesson, I learned to access data that is stored within an array using bracket notation.  Bracket notation works very similarly for an array as it does for strings.
+
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+
+var myData = myArray[0];
