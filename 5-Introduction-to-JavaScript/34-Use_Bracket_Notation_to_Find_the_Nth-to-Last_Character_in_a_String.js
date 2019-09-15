595f57bf18ebbad82c5a4ce03bb10ac9af36ca56
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 22:27:42 -0600

    Select second-to-last letter of last name
    Select the second-to-last letter of the last name using bracket notation and the length property.  Add a comment explaining how to select the Nth-to-last character of a string.

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+//This lesson expands upon the last lesson by introducing the idea that the Nth-to-last character of a string can be selected using the expression "variableName - N" inside the brackets.
+
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
