789ab452243bec64c619f334bc2f33743b4fa748
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 19:17:29 -0600

    Concatenate strings with += and variables
    Use both the augmented addition operator and a variable for a desired adjective to create a new concatenated string.  Add a comment explaining what I learned in this lesson about string concatenation.

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,12 @@
+//This lesson taught me how to use both the augmented addition operator and variables with string data types in concatenation.
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "easy!";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
