c443e54830cda03c12c61d0895d9f6f34f695bc8
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 22:45:17 -0600

    Nest two arrays within myArray
    Nest two arrays within the array myArray.  Add a comment explaining that it is possible to nest one or more arrays inside of another array.

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,7 @@
+//This lesson introduces the fact that JavaScript allows an array to serve as an element of another array; that is, one array can be nested within another array.
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["Life, the universe, and everything", 42], ["My niece's age", 4]];
