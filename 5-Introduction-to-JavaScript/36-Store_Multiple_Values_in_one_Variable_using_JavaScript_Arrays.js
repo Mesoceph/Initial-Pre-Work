a2803f1eb7a4ee1642d57bb454d5e867e3fe096e
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 22:41:56 -0600

    Create and initialize array called myArray
    Create a new array, myArray, and initialize it with a string value followed by a number value.  Add a comment explaining what this lesson taught me about what arrays are in JavaScript.

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,7 @@
+//This lesson introduces the concept of arrays.  An array is a single variable that contains multiple values.  The values may be of multiple data types.
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["The life, the universe, and everything", 42];
