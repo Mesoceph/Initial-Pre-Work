8aba7e20d6a41467897a3f9353f6067944e866b1
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 18:23:04 -0600

    Improve efficiency with augmented division operator
    Use the augmented division (/=) operator to simultaneously divide the current value of each variable by a specified value and then assign the quotient to that variable as its new value.  Add a comment explaining how the augmented division operator (/=) works.

---
+++ 5-Introduction-to-JavaScript/20-Compound-Assignment-With-Augmented-Division.js
@@ -0,0 +1,11 @@
+//In this lesson, I learned to use the augmented division operator (/=), which works in the same way as the augmented addition, subtraction, and multiplication operators.
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a /= 12;
+b /= 4;
+c /= 11;
