0c1fdbbdc74426493b1e21543c594c4a710b9d0f
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 19:38:51 -0600

    Fix typo in "Hello World" string
    Fix the typo in the "Hello World" string by assigning a new, correct string to the variable myStr.  Also, add a comment explaining what I learned in this lesson about string immutability.

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,8 @@
+//This lesson explains that bracket notation cannot be used to change a single character of a literal string.  Instead, the variable must be replaced by assigning a new string to the variable.
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
