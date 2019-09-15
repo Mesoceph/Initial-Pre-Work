41cdc212ae9cbc954b7e27cdc5243148f92dfdef
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 18:49:19 -0600

    Initialize myStr with concatenated strings.
    Initialize myStr with concatenated strings to produce the final string, "This is the start. This is the end."  Add a comment explaining how string concatenation works in JavaScript.

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,8 @@
+//This lesson reviews how to concatenate strings using the plus (+) operator.  Of note is that spaces need to be added at the beginning or end of the concatenated strings if the final string should have space between the concatenated strings.
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start." + " This is the end.";
