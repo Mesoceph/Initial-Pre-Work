07550b3f1fb80dcedabe9b941f8482c1ef2c90d1
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 19:10:53 -0600

    Concatenate strings using variables
    Create and initialize the new variable myStr to have the value "My name is [name] and I am well!" in which the value of [name] is supplied by the value of the variable myName.  Add a comment explaining how variables can be used in string concatenation.

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+//This lesson explains how to complete a concatenation operation including the string value of another variable.
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Marie";
+var myStr = "My name is " + myName + " and I am well!";
