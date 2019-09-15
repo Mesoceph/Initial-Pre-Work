6af92a482468c12d487f8d0592d20d5757d5298a
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 18:31:43 -0600

    Initialize myStr to string including double quotes
    Create the variable myStr and initialize it to the following string: I am a "double quoted" string inside "double quotes".  Add a comment explaining how to use the backslash character to add double quotes to a string without breaking the string.

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,3 @@
+//In this lesson, I learned to create a string containing double quotes by using the backslash (\) character before the quotes within the string to indicate that the quotes are a part of the string.
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
