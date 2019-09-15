19428a122461566ef8b2e0a41e602a22f680c39c
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 23:11:25 -0600

    Pop final nested array and move to new variable
    Pop the final array nested within myArray and use it to initialize the new variable removedFromMyArray.  Add a comment explaining how the pop() function works.

---
+++ 5-Introduction-to-JavaScript/42-Manipulate_Arrays_With_pop().js
@@ -0,0 +1,12 @@
+//This lesson taught me the pop() function, which removes the final value from an array and returns that value so that it can be assigned to a variable or used in an operation.
+
+// Example
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.pop();
