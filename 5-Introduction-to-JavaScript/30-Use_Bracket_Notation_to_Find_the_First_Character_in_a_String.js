780972a49bfc8992a25692df9d44f2fbbc19fcaa
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 19:33:03 -0600

    Find first character of last name
    Find the first character of the last name string stored in the lastName variable.  Add a comment explaining how to use bracket notation to find a specific character in a string.

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,14 @@
+//This lesson explains how bracket notation can be used to designate a specific character in a string.  It is important to note that the counting for character position starts at 0, not at 1.  Bracket notation uses the syntax variableName[characterr number].
+
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
