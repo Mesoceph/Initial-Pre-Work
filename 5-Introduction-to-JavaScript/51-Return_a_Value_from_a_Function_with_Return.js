4ddf3839d26183a20cba4a4d9c5c01df950a7b75
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 23:59:56 -0600

    Create function for multiplication of argument by 5
    Create a function that multiplies the argument by five and returns the product.  Add a comment explaining how to use return statements to retrieve values from functions.

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,15 @@
+//In this lesson, I learned how to use a return statement to retrieve a value produced by a function.  The value from a return statement can be assigned to a variable to store the results.
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+
+function timesFive(num) {
+  return num * 5;
+}
+
+console.log(timesFive(0));
+console.log(timesFive(5));
