954db857858f3dc5a04a5201f32a8b3072bfbe21
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 23:24:55 -0600

    Create shopping list as multi-dimensional array
    Create a multi-dimensional array to store my shopping list.  Every item in the array is an array containing first the item name as a string and then the number of that item that I need to purchase as a number.  Add a comment explaining what I learned in this lesson.

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,3 @@
+//In this lesson, I applied what I have learned in the last few lessons to create a multi-dimensional array containing my shopping list.
+
+var myList = [["Cat food can", 30], ["Head of lettuce", 1], ["Tomato", 5], ["Carrot", 3], ["Bottle of salad dressing", 1]];
