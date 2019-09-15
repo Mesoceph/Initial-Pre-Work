48a7a7eede66b3ab60b37103c8ca16db56dd6e07
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 22:56:09 -0600

    Modify array data from myArray
    Modify the first value stored in myArray to have the value of 45.  Add a comment explaining what I learned in this lesson about modifying data in arrays using indexes.

---
+++ 5-Introduction-to-JavaScript/39-Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,12 @@
+//This lesson demonstrates that, unlike strings, arrays are mutable and so an individual value can be changed using the indexing described in the last lesson.
+
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+
+myArray[0] = 45;
