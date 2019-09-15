2a78630d46a3f8c4b964e60ee75b63d5598c694d
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 22:18:52 -0600

    Select 3rd character of last name
    Select the third character of the last name using bracket notation and assign that character to the variable thirdLetterOfLastName.  Add a comment explaining how bracket notation can be used to select characters other than the first character.

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,11 @@
+//This lesson covers how to use bracket notation to find a character other than the first character.  As mentioned before, the first character is character 0, the second is character 1, etc.
+
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
