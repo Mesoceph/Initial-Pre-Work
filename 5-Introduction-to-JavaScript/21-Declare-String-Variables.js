51c27bea86da2171614aaed4198e0334ec750149
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 18:26:09 -0600

    Initialize new string variables
    Create two new string variables, myFirstName and myLastName, and initialize them with my first and last name.  Add a comment explaining how to use single or double quotes to indicate a string.

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,10 @@
+//This lesson explains how to initialize or assign string values using single or double quotes to surround the string.
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+
+var myFirstName = "Marie";
+var myLastName = "Hoerner";
