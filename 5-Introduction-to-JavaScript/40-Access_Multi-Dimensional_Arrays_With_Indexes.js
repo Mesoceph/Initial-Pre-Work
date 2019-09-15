9109585b5479bf6a190ed118da9d480b9ee1a461
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 23:04:27 -0600

    Access data within multi-dimensional arrray
    Access the data stored within a multi-dimensional array and use it to initialize the variable myData.  Add a comment explaining how data stored within multi-dimensional arrays can be accessed.

---
+++ 5-Introduction-to-JavaScript/40-Access_Multi-Dimensional_Arrays_With_Indexes.js
@@ -0,0 +1,8 @@
+//This lesson shows how to access data stored within nested arrays, called multi-dimensional arrays.  The format is to list the index for each array starting at the first level or outer-most array in brackets, followed by the index for the second level or second-to-outside array in brackets, etc.
+
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];
+ 
