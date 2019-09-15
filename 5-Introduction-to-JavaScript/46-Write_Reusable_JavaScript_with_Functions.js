acebc4501613c16a6907ea0b65c5d959e0cef66f
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 23:28:53 -0600

    Create and call function to print "Hi World"
    Create a new function called reusableFunction to print "Hi World" to the development console.  Add a comment explaining what I learned in this lesson about creating and using functions in JavaScript.

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,16 @@
+//This lesson explains how to program a function in JavaScript.  A function is a reusable piece of code that operates on the parameters (if any) that are provided when calling the function.
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+
+function reusableFunction() {
+    console.log("Hi World");
+}
+
+reusableFunction();
