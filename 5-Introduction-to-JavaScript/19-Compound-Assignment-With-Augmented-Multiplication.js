8222e436f7d268b5a4252fc35d75046c57496326
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 18:20:40 -0600

    Improve efficiency with augmented multiplication operator
    Use the augmented multiplication (*=) operator to simultaneously multiply a specified value by the current value of each variable and then assign the product to that variable as its new value.  Add a comment explaining how the augmented multiplication operator (*=) works.

---
+++ 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
@@ -0,0 +1,11 @@
+//This lesson covers the augmented multiplication operator, which works in the same way as the augmented addition and subtraction operators.
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a *= 5;
+b *= 3;
+c *= 10;
