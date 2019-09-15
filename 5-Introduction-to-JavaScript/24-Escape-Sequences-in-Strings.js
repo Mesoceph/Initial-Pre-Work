4493b214d8ffbfa678f133a74ec9b4122656838d
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 18:45:07 -0600

    Create string with line breaks, backslash, tab
    Create a single string that contains two line breaks, a tab, and a backslash using escape sequences to represent the special characters.  Add a comment explaining proper usage of escape sequences.

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,3 @@
+//In this lesson, I learned to use escape sequences to add special characters in JavaScript that either cannot be typed out (such as backspace) or would confuse JavaScript about the correct ending point of the string.
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
