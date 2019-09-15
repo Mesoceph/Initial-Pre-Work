fd1e2e018f88acec78f98d66d1100b339787d929
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 18:53:39 -0600

    Concatenate strings with augmented addition
    Concatenate strings with the augmented addition operator (+=) to give the variable myStr the final value of "This is the first sentence. This is the second sentence." Add a comment explaining how the augmented addition operator can be used to concatenate strings.

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,10 @@
+//This lesson taught me how to use the augmented addition operator (+=) to concatenate strings.
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
