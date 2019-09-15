6c63f0cd31b674f50c9292205c02079d5e5e66c7
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 18:18:04 -0600

    Improve efficiency with augmented subtraction operator
    Use the augmented subtraction (-=) operator to simultaneously subtract a value from the current value of each variable and then assign the difference to that variable as its new value.  Add a comment explaining how the augmented subtraction operator (-=) works.

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,11 @@
+//This lesson covers how to use the augmented subtraction operator (-=) to both calculate a difference and assign the difference to the variable at the same time.
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
