a9a00673a9b338e1a95495d2ac8bb1d0f2bf7328
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 22:24:08 -0600

    Select last character of last name
    Select the last character of the last name and use that character to initialize the variable lastLetterOfLastName.  Add a comment explaining how to select the last character of a string using bracket notation and the length property.

---
+++ 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+//In this lesson, I learned how to use the length property combined with bracket notation to select the last character in a string.  To select the last character, use the expression variableName.length - 1 inside the brackets.
+
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length-1];
