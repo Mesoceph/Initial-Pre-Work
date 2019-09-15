ea5a66a58250744b7f5f8b99140f1768d21a1d32
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 18:37:22 -0600

    Improve efficiency using single and double quotes
    Improve the efficiency of the code by using single quotes to define the string so that escape characters are not necessary for all of the double quotes within the string.  Add a comment explaining how single and double quotes work for defining strings in JavaScript.

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,3 @@
+//This lesson explains that both double and single quotes can be used to define a string.  This is useful to minimize the use of escape characters (/).  The escape character can still be used if the string contains both single and double quotes.
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
