6463e28e5f9094c55b8da44e4619fd6483170383
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-14 18:15:18 -0600

    Improve efficiency with compound addition/assignment operator
    Use the += operator to simultaneously add a value to the current value of each variable and then assign the sum to that variable as its new value.  Add a comment explaining how the compound addition/assignment operator (+=) works.

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,11 @@
+//This lesson reviews the compound operator for assignment and addition (+=).  Using this operator will add a value to the current value of the variable and assign the sum to the same variable.
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
