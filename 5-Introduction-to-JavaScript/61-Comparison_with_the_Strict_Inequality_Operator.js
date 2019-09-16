ab09e31cd3ddf1e85bc2debc835adf7d60443d75
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-16 12:34:16 -0600

    Create function to identify values strictly equal to 17
    Create a function that will identify whether or not arguments are strictly equal to 17 and return the statement "Equal" or "Not Equal" as appropriate.  Add a comment explaining how the strict inequality operator (!==) functions.

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,17 @@
+//This lesson introduces the strict inequality operator (!==), which is the opposite of the strict equality operator.  If the two values are not strictly equal, then it returns a value of true.
+
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+
+  if (val !== 17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(10);
