b2f9a14c97e530e71ac8ad65e30bfea22498a56d
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-15 22:33:05 -0600

    Create Boolean function to return value true
    Create a Boolean function that returns the value true when called.  Add a comment explaining what Boolean values are.

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,10 @@
+//This lesson introduces the concept of Boolean (true or false) values.  It is important to recognize that Boolean values are not strings and do not require quotes.
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
