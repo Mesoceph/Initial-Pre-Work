03c148c1d8202ba1de709903656d18bce4fce9e0
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-20 11:06:57 -0600

    Improve code efficiency with less than operator
    Improve the efficiency of the code by replacing an if/else pair of statements with a single comparison statement.  Add a comment explaining how comparison operators can be used to return boolean values and improve code efficiency.

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,8 @@
+//In this lesson, I learned a technique for making code more efficient using comparison operators.  Rather than using an if/else pair of statements to return true or false values, a comparison statement can be used by itself to return the true/false values.
+
+function isLess(a, b) {
+  // Fix this code
+  return a < b;
+}
+// Change these values to test
+isLess(10, 15);
