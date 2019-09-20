091942fb97a529202727686e6d2e019a6e588163
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-20 11:37:13 -0600

    Create object myDog to describe dog
    Create the object myDog to describe the properties of my dog, including her name, number of legs and tails, and her friends.  Add a comment explaining what JavaScript objects are and describing their similarities and differences when compared to arrays.

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,18 @@
+//This lesson introduces JavaScript objects.  Objects are similar to arrays, but instead of using indexes to organize and access the data, objects use properties.  Properties can be numbers, strings, and arrays.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Slippers",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Cats", "Dogs", "Children"]
+};
