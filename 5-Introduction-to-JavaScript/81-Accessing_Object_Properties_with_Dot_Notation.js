6df7865551f23bcbb77c9f84901cdc3778be9d56
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-20 11:42:51 -0600

    Access property values from testObj
    Use dot notation to access property values stored in the object testObj.  Add a comment explaining how dot notation works.

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,13 @@
+//This lesson introduces dot notation for accessing property values stored in an object.  Dot notation consists of the object name followed by a period and then the property name, such as objectName.propertyName.
+
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
