08076037025033ba1e3a9a3abe09e2f7feee21f7
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-20 16:14:49 -0600

    Add record to myMusic for "Carry On Wayward Son"
    Add a record to the array myMusic by creating a new object for the song "Carry On Wayward Son."  Add a comment describing how complex data structures can be created by nesting objects and arrays together.

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,26 @@
+//In this lesson, I learned to nest objects within arrays, and then to nest arrays within the objects as well.  The ability to nest objects and arrays within each other provides flexibility iin creating complex data structures.
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Kansas",
+    "title": "Carry on WayWard Son",
+    "release_year": 1976,
+    "formats": [
+      "7-inch single",
+      "LP"
+    ],
+    "gold": true
+  }
+  // Add record here
+];
