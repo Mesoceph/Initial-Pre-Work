ba13efd8df56ecd916cc6767f6b2cbbde3502e0f
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-20 15:08:53 -0600

    Add 'bark' property to myDog object
    Add a property called "bark" with the value "woof" to the object myDog.  Add a comment explaining how properties can be added to JavaScript objects.

---
+++ 5-Introduction-to-JavaScript/85-Add_New_Properties_to_a_JavaScript_Object.js
@@ -0,0 +1,23 @@
+//In this lesson, I learned how to add new properties to a JavaScript object.  To do so, the notation is the same as for updating the value in an existing property.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+
+myDog.bark = "woof";
