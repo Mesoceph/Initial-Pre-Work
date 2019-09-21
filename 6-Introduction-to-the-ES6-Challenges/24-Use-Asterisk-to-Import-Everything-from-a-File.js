928f2908540db4f88fbda4db99eb0800934dea8d
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-21 13:56:32 -0600

    Import all contents from capitalize_strings
    Import all contents from the file capitalize_strings to a new object called capitalizeStrings.  Add a comment explaining the import * syntax for importing all contents.

---
+++ 6-Introduction-to-the-ES6-Challenges/24-Use-Asterisk-to-Import-Everything-from-a-File.js
@@ -0,0 +1,5 @@
+//This lesson builds on the last two lessons by introducing import * statements.  Using the asterisk will import all of the file contents into the current file.  The syntax is: import * as newObjectName from "fileName".  As woith other import statements, the file name must also contain the file path if it is not in the same directory as the current file.
+
+"use strict";
+
+import * as capitalizeStrings from "capitalize_strings";
