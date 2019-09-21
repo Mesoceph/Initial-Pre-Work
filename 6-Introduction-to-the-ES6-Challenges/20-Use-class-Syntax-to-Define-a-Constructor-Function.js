22d39b183f129cf390bc1b1b94c6974e3e07db24
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-21 13:13:35 -0600

    Create Vegetable constructor function with name property
    Create a Vegetable constructor function so that the objects created have the property name with the value equal to the argument of the Vegetable function.  Add a comment explaining what I learned in this lesson about the new syntax using the class keyword for defining constructor functions available in ES6.

---
+++ 6-Introduction-to-the-ES6-Challenges/20-Use-class-Syntax-to-Define-a-Constructor-Function.js
@@ -0,0 +1,16 @@
+//This lesson introduces the syntax to create objects using the keyword class combined with a constructor function.
+
+function makeClass() {
+  "use strict";
+  /* Alter code below this line */
+  class Vegetable {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  /* Alter code above this line */
+  return Vegetable;
+}
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // => should be 'carrot'
