8eec26251c6f0853f4f5b604bd57ab3e84e9d7e7
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-20 20:43:40 -0600

    Create loop to push numbers 0-4 to myArray
    Create a while loop to push the numbers zero through four to the array myArray.  Add a comment explaining how while loops work.

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,12 @@
+//In this lesson, I learned to use while loops in JavaScript.  The loop will continue to iterate until the condition is no longer true.
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+var i = 0;
+while(i < 5) {
+  myArray.push(i);
+  i++;
+}
