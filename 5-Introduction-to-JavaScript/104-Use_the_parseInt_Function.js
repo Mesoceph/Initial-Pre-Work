9ab1e524d1db246cdc844edf53e9e37e6732b804
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-20 22:03:43 -0600

    Create function to parse strings and return integers
    Create a function to parse a string argument and return an integer value.  Add a comment explaining how the parseInt() function works.

---
+++ 5-Introduction-to-JavaScript/104-Use_the_parseInt_Function.js
@@ -0,0 +1,7 @@
+//This lesson introduces the parseInt function, which parses a string and returns an integer value.  If the function cannot convert the first character to a number, the function returns NaN (not a number).
+
+function convertToInteger(str) {
+  return parseInt(str);
+}
+
+convertToInteger("56");
