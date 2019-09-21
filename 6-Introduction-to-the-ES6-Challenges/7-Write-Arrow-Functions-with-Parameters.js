fb01b70f62cd3c95d365f561f26cac8f8d3e8333
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-20 23:13:11 -0600

    Simplify code for myConcat using arrow function
    Simplify the code used in the declaration of myConcat using arrow function syntax.  Change the declaration to use the const keyword instead of the var keyword so that myConcat cannot be overridden.  Also, add a comment explaining proper usage of parameters in arrow functions.

---
+++ 6-Introduction-to-the-ES6-Challenges/7-Write-Arrow-Functions-with-Parameters.js
@@ -0,0 +1,6 @@
+//In this lesson, I learned how to use arrow syntax for functions that take arguments.  The paramenter(s) is/are included inside the parentheses before the arrow.
+
+const myConcat = (arr1, arr2) => arr1.concat(arr2)
+
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
