1b3a173ce2b0b3241bae05691466e375ffd47fe3
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-20 19:55:13 -0600

    Create variable for second record in tree variety array
    Create a variable with the value of the second tree in the array "list" within the object for trees in the myPlants object.  Add a comment explaining what I learned in this lesson about how to access data from nested arrays.

---
+++ 5-Introduction-to-JavaScript/91-Accessing_Nested_Arrays.js
@@ -0,0 +1,25 @@
+//In this lesson, I learned to access arrays that are nested within objects using array bracket notation and object dot notation.
+
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1]; // Change this line
