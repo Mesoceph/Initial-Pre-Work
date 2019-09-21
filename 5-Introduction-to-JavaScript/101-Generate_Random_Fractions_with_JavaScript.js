6f9ab9519f4f609d6fba580ab937dceee55a0b98
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-20 21:39:12 -0600

    Create function to return random number in [0,1)
    Create a function to return a random number between 0 (inclusive) and 1 (exclusive).  Add a comment explaining what I learned in this lesson about using the Math.random() function in JavaScript.

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,10 @@
+//This lesson introduces the Math.random() function that returns a random number between 0 (inclusive) and 1 (exclusive).
+
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
