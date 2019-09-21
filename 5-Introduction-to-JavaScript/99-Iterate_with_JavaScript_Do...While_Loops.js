cc0ea219b019219b1548b3a3c16eae9ff8defb1f
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-20 21:24:32 -0600

    Push number 10 to myArray
    Push the number 10 to myArray with a do...while loop.  Add a comment explaining how do...while loops work and describing their similarities and differences when compared with while loops.

---
+++ 5-Introduction-to-JavaScript/99-Iterate_with_JavaScript_Do...While_Loops.js
@@ -0,0 +1,12 @@
+//This lesson introduces do...while loops.  These loops work the same way as regular while loops except that the first iteration of the loop runs regardless of whether or not the loop condition is true.
+
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+
+do {
+  myArray.push(i);
+  i++;
+} while (i < 5)
