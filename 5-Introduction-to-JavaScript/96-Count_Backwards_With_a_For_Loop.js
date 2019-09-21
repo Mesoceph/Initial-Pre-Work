13da3ddbaa18889b995bea46492bd25e3249051c
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-20 21:05:44 -0600

    Create loop to push odd numbers to myArray in decreasing order from 9-1
    Create a loop to push odd numbers in decreasing order to myArray, starting with 9 and ending with 1.  Add a comment explaining how to decrement the loop variable in order to "count backwards."

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,17 @@
+//In this lesson, I learned how to decrement rather than increment the for loop variable to allow the variable to count down rather than count up.
 
+// Example
+var ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+for (i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
