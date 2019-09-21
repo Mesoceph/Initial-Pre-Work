bccc9545097320500c76ca1116e12d6ddf2f625b
Mesoceph <31560397+Mesoceph@users.noreply.github.com>
2019-09-21 14:04:09 -0600

    Import subtract function from math_functions file
    Import the subtract function from the math_functions file stored in the same directory as the current file.  Add a comment explaining how to import default exports from another file.

---
+++ 6-Introduction-to-the-ES6-Challenges/26-Import-a-Default-Export.js
@@ -0,0 +1,6 @@
+//In this lesson, I learned the difference in syntax for importing a default export.  For a default export, the name of the value to import is not surrounded by curly braces.
+
+"use strict";
+
+import subtract from "math_functions";
+subtract(7,4);
